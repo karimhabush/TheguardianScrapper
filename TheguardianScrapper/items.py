66e530e21e3a04d26a051cf290959b89ac151b92
Huh <karim.habush@gmail.com>
2020-02-14 20:06:20 +0100

    Add class ArticleItem for processing

--- TheguardianScrapper/items.py
+++ TheguardianScrapper/items.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
+class ArticleItem(scrapy.Item):
+    author = scrapy.Field()
+    headline = scrapy.Field()
+    content = scrapy.Field()
+    standfirst = scrapy.Field()
+    label = scrapy.Field()
+    url = scrapy.Field()
+    published_at = scrapy.Field()
 
-class TheguardianscrapperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
